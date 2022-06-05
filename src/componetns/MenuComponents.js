96f9d5db1777d946703db3a6f97939492b37807a
Tran Đưc Huy <huytran.1912199&@gmail.com>
2022-06-05 11:24:45 +0700

    6re Update

--- src/componetns/MenuComponents.js
+++ src/componetns/MenuComponents.js
@@ -13,10 +13,11 @@ let { column2, column3, column4, column6 ,column1} = classChange;
 
 class StaffList extends Component {
   constructor(props) {
-    super(props);
+      super(props);
+      
     this.state = {
       onSelectedStaff: null,
-      classDefault: "col-md-3 mt-1"
+      classDefault:column4
     };
   }
 
@@ -32,26 +33,24 @@ class StaffList extends Component {
     });
   }
 
-  renderStaff(staff) {
+    renderStaff(staff) {
+      
     if (staff != null) {
       return (
         <div className="col-12">
           <Card>
             <CardBody style={{backgroundColor:"RGBA( 245, 245, 220, 1 )"}}>
               <CardTitle>Họ và tên: {staff.name}</CardTitle>
-              <CardText>
-                Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}
-              </CardText>
-              <CardText>
-                Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}
-              </CardText>
+              <CardText>Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}</CardText>
+              <CardText> Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")} </CardText>
               <CardText>Phòng ban: {staff.department.name}</CardText>
               <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
               <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
             </CardBody>
           </Card>
         </div>
-      );
+        );
+        
     } else {
       return <div></div>;
     }
@@ -59,12 +58,14 @@ class StaffList extends Component {
 
   render() {
     
-    const staffList = this.props.staffs.map((staff) => {
-      return (
+      const staffList = this.props.staffs.map((staff) => {
+        
+          
+        return (
         <div className={this.state.classDefault}>
-          <Card key={staff.id} onClick={() => this.onStaffSelect(staff)}>
+            <Card key={staff.id} onClick={() => this.onStaffSelect(staff)}>
             <CardBody>
-              <CardTitle>{staff.name}</CardTitle>
+                <CardTitle>{staff.name}</CardTitle>
             </CardBody>
               </Card>
         </div>
@@ -75,29 +76,24 @@ class StaffList extends Component {
             <div className="container">
             <div className="row m-3">
                 
-          <button onClick={() => this.onColumnSelect(column1)}className="btn  btn-info mr-3"> 1 cột</button>
-          <button onClick={() => this.onColumnSelect(column2)}className="btn  btn-info mr-3"> 2 cột</button>
-          <button onClick={() => this.onColumnSelect(column3)}className="btn  btn-info mr-3"> 3 cột</button>
-          <button onClick={() => this.onColumnSelect(column4)}className="btn  btn-info mr-3"> 4 cột</button>
-          <button onClick={() => this.onColumnSelect(column6)}className="btn  btn-info mr-3"> 6 cột</button>
+                <button onClick={() => this.onColumnSelect(column1)}className="btn  btn-info mr-3"> 1 cột</button>
+                <button onClick={() => this.onColumnSelect(column2)}className="btn  btn-info mr-3"> 2 cột</button>
+                <button onClick={() => this.onColumnSelect(column3)}className="btn  btn-info mr-3"> 3 cột</button>
+                <button onClick={() => this.onColumnSelect(column4)}className="btn  btn-info mr-3"> 4 cột</button>
+                <button onClick={() => this.onColumnSelect(column6)}className="btn  btn-info mr-3"> 6 cột</button>
           
-        </div>
-              <div className="row">{staffList}
+            </div>
+              
+            <div className="row">{staffList}
                   
-                  <div style={{
-                      fontSize: "40px",
-                      color:"pink"
-                  
-                  }}>
-            
-                      Bấm vào nhân viên để xem thông tin
-                      
-              </div>
-              </div>
-        <div className="row mt-5">
-          {this.renderStaff(this.state.onSelectedStaff)}
-        </div>
-      </div>
+                  <div style={{fontSize: "40px",color:"pink"  }}>
+                      Bấm vào nhân viên để xem thông tin 
+            </div>
+            </div>
+            <div className="row mt-5">
+                    {this.renderStaff(this.state.onSelectedStaff)}
+            </div>
+            </div>
     );
   }
 }
