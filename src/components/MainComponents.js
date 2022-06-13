be47a2ec291e47688faf59f2a102c5a8654bf21d
Tran Đưc Huy <huytran.1912199&@gmail.com>
2022-06-13 08:45:18 +0700

    Bài ASM3 Nâng Cao

--- src/components/MainComponents.js
+++ src/components/MainComponents.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
-import StaffList from "./StaffList"
+import StaffList from "./StaffListComponen"
 import Department from "./DepartmentComponent";
 import Detail from "./DetailComponents";
 import SalaryDetail from "./SalaryComponent";
@@ -9,32 +9,51 @@ import{Switch,Route,Redirect} from "react-router-dom"
 import{STAFFS,DEPARTMENTS}from "../shared/Staffs"
 
 
-function Main() {
-    const [state, setState] = useState({
-        staffs: STAFFS,
-        departmens:DEPARTMENTS
-    })
-
-    const DetailStaff = ({ match }) => {
-        return (
-            <Detail ID={ state.staffs.filter((item)=>item.id===parseInt(match.params.staffId,10))[0]}/>
-        )
-
+class Main extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+             staffs: STAFFS,
+            departmens:DEPARTMENTS
+        }
+        this.addStaff=this.addStaff.bind(this)
     }
+    // Thêm nhân viên vào danh sách
 
-    return (
+    addStaff(staff) {
+        const id = Math.floor(Math.random() * 10000 + 1);
+        const newStaff = { id, ...staff };
+        this.setState({
+            staffs:[...this.state.staffs,newStaff]
+        })
+    }
+    render() {
+        const DetailStaff = ({ match }) => {
+            return (
+                <Detail ID={this.state.staffs.filter((item) => item.id === parseInt(match.params.staffId, 10))[0]} />
+            )
+        }
+         return (
         <div>
             <Header />
             <Switch>
-                <Route  exact path='/staff' component={() => <StaffList staffs={state.staffs} />} />
-                <Route  path='/staff/:staffId' component ={DetailStaff}/>
-                <Route  path='/department' component ={()=><Department department={state.departmens}/>}/>
-                <Route path='/salary' component={() => <SalaryDetail salary={state.staffs} />} />
+                <Route  exact path='/staff' component={() => <StaffList addStaff={this.addStaff} staffs={this.state.staffs} />} />
+                <Route path='/staff/:staffId' component={DetailStaff} />
+                <Route  path='/department' component ={()=><Department department={this.state.departmens}/>}/>
+                <Route path='/salary' component={() => <SalaryDetail salary={this.state.staffs} />} />
                 <Redirect to="/staff"/>
             </Switch>
             <Footer/>
         </div>
     )
-}
+    
+    }
+       
+    }
+
+   
+
+   
+
 
 export default Main
---
+++ src/components/StaffListComponen.js
@@ -0,0 +1,317 @@
+import React, { Component } from "react";
+import {
+  Card,
+  CardImg,
+  CardBody,
+  CardSubtitle,
+  Button,
+  Modal,
+  Col,
+  Input,
+  ModalHeader,
+  ModalBody,
+  Row,
+  Label,
+  FormFeedback
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import { Control, LocalForm, Errors } from "react-redux-form";
+
+// Điều kiện cho form dữ liệu
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+const isNumber = (val) => !isNaN(Number(val));
+
+
+// Render danh sách nhân viên
+const RenderStaffItem = ({ staff }) => {
+  return (
+    <Link to={`/staff/${staff.id}`}>
+      <Card>
+        <CardImg width="100%" src={staff.image} alt={staff.name} />
+        <CardBody>
+          <CardSubtitle>{staff.name}</CardSubtitle>
+        </CardBody>
+      </Card>
+    </Link>
+  );
+};
+
+
+
+class StaffList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nameF: "",
+      modalOpen: false,
+      doB: "",
+      startDate: "",
+      touched: {
+        doB: false,
+        startDate: false
+      }
+    };
+
+    this.toggleModal = this.toggleModal.bind(this);
+    this.findStaff = this.findStaff.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  // 
+  handleBlur = (field) => (evt) => {
+    this.setState({
+      touched: { ...this.state.touched, [field]: true }
+    });
+  };
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit = (value) => {
+    const newStaff = {
+      name: value.name,
+      doB: this.state.doB,
+      startDate: this.state.startDate,
+      department: value.department,
+      salaryScale: value.salaryScale,
+      annualLeave: value.annualLeave,
+      overTime: value.overTime,
+      image: "/assets/images/alberto.png"
+    };
+    if (!this.state.doB || !this.state.startDate)
+      this.setState({
+        touched: { doB: true, startDate: true }
+      });
+    else this.props.addStaff(newStaff);
+  };
+// Điều kiện dữ liệu cho Ngày sinh, và ngày làm việc
+  validate(doB, startDate) {
+    const errors = {
+      doB: "",
+      startDate: ""
+    };
+
+    if (this.state.touched.doB && doB.length < 1) errors.doB = "Yêu cầu nhập";
+    if (this.state.touched.startDate && startDate.length < 1)
+      errors.startDate = "Yêu cầu nhập";
+
+    return errors;
+  }
+// Ẩn hiện Form
+  toggleModal() {
+    this.setState({
+      modalOpen: !this.state.modalOpen
+    });
+  }
+
+  // Tìm kiếm nhân viên
+  findStaff(event) {
+    const nameS = event.target.nameS.value;
+    event.preventDefault();
+    this.setState({ nameF: nameS });
+  }
+
+  render() {
+    const errors = this.validate(this.state.doB, this.state.startDate);
+
+    const staffList = this.props.staffs
+      .filter((val) => {
+        if (this.state.nameF === "") return val;
+        else if (
+          val.name.toLowerCase().includes(this.state.nameF.toLowerCase())
+        )
+          return val;
+        return 0;
+      })
+      .map((val) => {
+        return (
+          <div className="col-6 col-md-4 col-lg-2 mt-3 mb-3" key={val.id}>
+            <RenderStaffItem staff={val} />
+          </div>
+        );
+      });
+
+    //Render giao diện nhân viên
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-6 mt-3">
+            <div className="row">
+              <div className="col-10 col-md-10">
+                <h3>Nhân viên</h3>
+              </div>
+              <div className="col-2 col-auto">
+                <Button outline onClick={this.toggleModal}>
+                  <span className="fa fa-plus fa-lg"></span>
+                </Button>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 mt-3">
+            <form onSubmit={this.findStaff} className="form-group row">
+              <div className="col-8 col-md-8">
+                <input type="text"name="nameS" className="form-control"placeholder="Tìm kiếm nhân viên ..." />
+              </div>
+              <div className="col-4 col-md-4">
+                <button className="btn btn-primary" type="submit"> Tìm kiếm </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="col-12">
+          <hr />
+        </div>
+
+       {/* Render ra Form */}
+        <Modal isOpen={this.state.modalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Thêm nhân viên</ModalHeader>
+          <ModalBody>
+            <LocalForm onSubmit={(value) => this.handleSubmit(value)}>
+              <Row className="control-group">
+                <Label htmlFor="name" md={4}>
+                  Tên
+                </Label>
+                <Col md={8}>
+                  <Control.text model=".name" className="form-control"  id="name" name="name"
+                    validators={{
+                      required,
+                      minLength: minLength(3),
+                      maxLength: maxLength(30)
+                    }}
+                  />
+                  <Errors model=".name"  className="text-danger"  show="touched"
+                    messages={{
+                      required: "Yêu cầu  ",
+                      minLength: "nhập nhiều hơn 3 ký tự",
+                      maxLength: "Yêu cầu nhập ít hơn 30 ký tự"
+                    }}
+                  />
+                </Col>
+              </Row>
+
+              <Row className="control-group">
+                <Label htmlFor="doB" md={4}> Ngày sinh</Label>
+                <Col md={8}>
+                  <Input type="date" name="doB" id="doB"
+                    valid={errors.doB === ""}
+                    invalid={errors.doB !== ""}
+                    onBlur={this.handleBlur("doB")}
+                    onChange={this.handleInputChange}
+                  />
+                  <FormFeedback>{errors.doB}</FormFeedback>
+                </Col>
+              </Row>
+
+              <Row className="control-group">
+                <Label htmlFor="startDate" md={4}>Ngày vào công ty</Label>
+                <Col md={8}>
+                  <Input type="date" name="startDate" id="startDate"
+                    valid={errors.startDate === ""}
+                    invalid={errors.startDate !== ""}
+                    onBlur={this.handleBlur("startDate")}
+                    onChange={this.handleInputChange}
+                  />
+                  <FormFeedback>{errors.startDate}</FormFeedback>
+                </Col>
+              </Row>
+              <Row className="control-group">
+                <Label htmlFor="department" md={4}>
+                  Phòng ban
+                </Label>
+                <Col md={8}>
+                  <Control.select model=".department" name="department"id="department"
+                    defaultValue="Sale"className="form-control">
+                    <option>Sale</option>
+                    <option>HR</option>
+                    <option>Marketing</option>
+                    <option>IT</option>
+                    <option>Finance</option>
+                  </Control.select>
+                </Col>
+              </Row>
+              <Row className="control-group">
+                <Label htmlFor="salaryScale" md={4}>
+                  Hệ số lương
+                </Label>
+                <Col md={8}>
+                  <Control.text model=".salaryScale" id="salaryScale" name="salaryScale"
+                    placeholder="1.0 -> 3.0"  defaultValue="1"  className="form-control"
+                    validators={{
+                      required,
+                      isNumber,
+                   
+                    }}
+                  />
+                  <Errors model=".salaryScale"className="text-danger"show="touched"
+                    messages={{
+                      required: "Yêu cầu nhập",
+                      isNumber: "Phải là chữ số",
+                      
+                    }}
+                  />
+                </Col>
+              </Row>
+              <Row className="control-group">
+                <Label htmlFor="annualLeave" md={4}>Số ngày nghỉ còn lại</Label>
+                <Col md={8}>
+                  <Control.text model=".annualLeave"id="annualLeave"name="annualLeave" defaultValue="0"
+                    validators={{
+                      required,
+                      isNumber
+                    }}
+                    className="form-control"
+                  />
+                  <Errors model=".annualLeave"className="text-danger"show="touched"
+                    messages={{
+                      required: "Yêu cầu nhập",
+                      isNumber: "Phải là chữ số"
+                    }}
+                  />
+                </Col>
+              </Row>
+              <Row className="control-group">
+                <Label htmlFor="overTime" md={4}>
+                  Số ngày đã làm thêm
+                </Label>
+                <Col md={8}>
+                  <Control.text  model=".overTime"  id="overTime"  name="overTime" defaultValue="0"
+                    validators={{
+                      required,
+                      isNumber
+                    }}
+                    className="form-control"
+                  />
+                  <Errors  model=".overTime" className="text-danger"  show="touched"
+                    messages={{
+                      required: "Yêu cầu nhập",
+                      isNumber: "Phải là chữ số"
+                    }}
+                  />
+                </Col>
+              </Row>
+              <Row className="control-group">
+                <Col md={{ size: 10, offset: 2 }}>
+                  <Button type="submit" color="success"> Thêm </Button>
+                </Col>
+              </Row>
+            </LocalForm>
+          </ModalBody>
+        </Modal>
+
+        <div className="row shadow mb-5 mt-5">{staffList}</div>
+      </div>
+    );
+  }
+}
+
+export default StaffList;
